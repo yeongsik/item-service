651571210e1ccd135cc90a4aec0130c6ad82144c
LAPTOP-N91UPOS7\dudtl <youngsik0266@gmail.com>
2021-08-06 08:10:02 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,23 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity; // 수량이 없을 수도 있다고 가정해서 Integer 사용 , int -> null이 못 들어감
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); // static // HashMap 사용 X
+    private static long sequence = 0L; // static // 동시 접속할 때 데이터가 꼬일 수 있다.
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+
+//      중복이냐 명확성이냐 일때 명확성을 따르는게 좋다.
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
